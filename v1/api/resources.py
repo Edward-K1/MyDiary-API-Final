dd4268489fc9ba293457e8dc3dc8d0cc98b4a3e9
Edward-K1 <eddyk1collab@gmail.com>
2018-07-26 04:50:30 +0300

    Complete auth/login

--- v1/api/resources.py
+++ v1/api/resources.py
@@ -74,17 +74,25 @@ class UserLoginResource(Resource):
             "user_id": user_id
         }, app.config['SECRET_KEY'])
 
+        return make_response(
+            jsonify({
+                "message":
+                "Logged in successfully. Take note of your access-token. You'll need it to authenticate your requests",
+                "access-token":
+                encoded_token.decode('UTF-8')
+            }), 200)
+
 
 class DiaryResource(Resource):
     @token_required
-    def get(user_id,self):
+    def get(user_id, self):
         return make_response(
             jsonify({
-                "Diary Entries": DiaryEntry.get_all_diary_entries()
+                "Diary Entries": DiaryEntry.get_all_diary_entries(user_id)
             }), 200)
 
     @token_required
-    def post(user_id,self):
+    def post(user_id, self):
         data = request.get_json()
         result = validate_and_assemble_data(data, DIARY_ENTRY_FIELDS,
                                             DIARY_ENTRY_TYPES)
@@ -109,7 +117,7 @@ class DiaryResource(Resource):
 
 class DiaryEditResource(Resource):
     @token_required
-    def get(user_id,self, entryId):
+    def get(user_id, self, entryId):
         entry = DiaryEntry.get_single_entry(entryId)
 
         if not entry:
@@ -123,7 +131,7 @@ class DiaryEditResource(Resource):
         return make_response(jsonify({"Diary Entry": entry}), 200)
 
     @token_required
-    def put(user_id,self, entryId):
+    def put(user_id, self, entryId):
         data = request.get_json()
         result = validate_and_assemble_data(data, DIARY_ENTRY_FIELDS,
                                             DIARY_ENTRY_TYPES)
@@ -153,7 +161,7 @@ class DiaryEditResource(Resource):
             }), 201)
 
     @token_required
-    def delete(user_id,self, entryId):
+    def delete(user_id, self, entryId):
         entry = DiaryEntry.delete_entry(entryId)
         if entry:
             return make_response(
