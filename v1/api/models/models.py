bbe0968721e32ac78f493afc2f7a0e2217148cd9
Edward-K1 <eddyk1collab@gmail.com>
2018-07-30 18:19:37 +0300

    Update models after repo change

--- v1/api/models/models.py
+++ v1/api/models/models.py
@@ -16,6 +16,15 @@ class User(object):
 
     def save(self):
         """ Commit current instance of a user to the database """
+        username_check = dbm.check_username_exists(self.__username)
+        email_check = dbm.check_email_exists(self.__email)
+
+        if username_check[0]:
+            return "The selected username is not available", 409
+        else:
+            if email_check[0]:
+                return "The selected email already exists. Please log in.", 409
+
         result = dbm.insert_user(self.__firstname, self.__lastname,
                                  self.__username, self.__email,
                                  self.__password)
@@ -33,7 +42,7 @@ class User(object):
 
 class DiaryEntry(object):
 
-    db_labels= ("eid","title","content","created")
+    db_labels = ("eid", "title", "content", "created")
 
     def __init__(self, uid, title, content):
         """
@@ -49,35 +58,33 @@ class DiaryEntry(object):
         """
         return dbm.insert_diary_entry(self.__uid, self.__title, self.__content)
 
-
     @staticmethod
-    def jsonify_and_attach_labels(items:list):
+    def jsonify_and_attach_labels(items: list):
         return {
-        DiaryEntry.db_labels[0]:items[0],
-        DiaryEntry.db_labels[1]:items[1],
-        DiaryEntry.db_labels[2]:items[2],
-        DiaryEntry.db_labels[3]:items[3]
+            DiaryEntry.db_labels[0]: items[0],
+            DiaryEntry.db_labels[1]: items[1],
+            DiaryEntry.db_labels[2]: items[2],
+            DiaryEntry.db_labels[3]: items[3]
         }
-
 
     @staticmethod
     def get_all_diary_entries(uid):
         """ Fetch the diary entries of a particular user """
         result = dbm.get_diary_entries(uid)
-        jsonified_list=[]
-        print(result)
+        jsonified_list = []
         for entry in result[0]:
             jsonified_list.append(DiaryEntry.jsonify_and_attach_labels(entry))
 
-        print(jsonified_list)
         return jsonified_list
 
     @staticmethod
     def get_single_entry(eid):
         """ Fetch a specific diary entry based on its id """
         entry = dbm.get_single_diary_entry(eid)
-        jsonified = DiaryEntry.jsonify_and_attach_labels(entry[0])
-        return jsonified,entry[1]
+        jsonified = ''
+        if isinstance(entry[0], tuple):
+            jsonified = DiaryEntry.jsonify_and_attach_labels(entry[0])
+        return jsonified, entry[1]
 
     @staticmethod
     def modify_entry(eid, title, content):
